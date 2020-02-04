9faf979bb977347b5eb0f9ec019a8a2d82463745
tautvydaskubolis <tautvydas.kubolis@unity3d.com>
2020-02-04 10:41:11 +0200

    stripped down package

--- Editor/AssetbundleBuilder/AssetbundleCreator.cs
+++ Editor/AssetbundleBuilder/AssetbundleCreator.cs
@@ -5,18 +5,9 @@ namespace Packages.MEMCoreUnity.Editor.AssetbundleBuilder
     public class AssetbundleCreator
     {
         [MenuItem("MEM/Build Assetbundles")]
-        public static void BuildAssetbundles(MenuCommand command)
+        public static void BuildAssetbundles()
         {
-            BuildAssetbundles(BuildTarget.StandaloneWindows64, BuildTargetGroup.Standalone, "/Assetbundles/");
-        }
-
-        public static void BuildAssetbundles(BuildTarget buildTarget, BuildTargetGroup buildGroup, string outputPath)
-        {
-#if UNITY_ADDRESSABLES
-            AddressablesBuilder.BuildAssetbundles(buildTarget, buildGroup, outputPath);
-#else
-            AssetbundleBuilder.BuildAssetbundles(buildTarget, buildGroup, outputPath);
-#endif
+            Debug.log("done");
         }
     }
 }
